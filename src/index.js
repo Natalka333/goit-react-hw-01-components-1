0b505a6537aa0b8bd0c5bbbac306a56ed5fe0bc8
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2023-09-08 21:15:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { App } from './components/App';
 import './index.css';
 
-// import friends from './components/dataAll.json/friends.json';
-// import data from './components/dataAll.json/data.json';
-// import transactions from './components/dataAll.json/transactions.json';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
